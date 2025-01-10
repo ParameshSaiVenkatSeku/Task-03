7455c42be0b8ccfabe76a3399eac0193b883e2b0
Sai Venkat <parameshsaivenkatseku@gmail.com>
2025-01-10 15:37:10 +0530

    first commit

---
+++ Work/frontEnd/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './features/login/components/login/login.component';
+import { RegisterComponent } from './features/register/components/register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
