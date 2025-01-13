d4eb07bf52d0d992d11e24b51507078bdecd1984
Sai Venkat <parameshsaivenkatseku@gmail.com>
2025-01-13 16:28:46 +0530

    components corrected

--- Work/frontEnd/src/app/app-routing.module.ts
+++ Work/frontEnd/src/app/app-routing.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './features/login/components/login/login.component';
-import { RegisterComponent } from './features/register/components/register/register.component';
+import { LoginComponent } from './features/login/login.component';
+import { RegisterComponent } from './features/register/register.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
   {
     path: '',
     component: LoginComponent,
     pathMatch: 'full',
   },
   {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
     path: 'register',
     component: RegisterComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
   },
 ];
 
--- Work/frontEnd/src/app/app.module.ts
+++ Work/frontEnd/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './features/login/components/login/login.component';
-import { RegisterComponent } from './features/register/components/register/register.component';
+import { LoginComponent } from './features/login/login.component';
+import { RegisterComponent } from './features/register/register.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
